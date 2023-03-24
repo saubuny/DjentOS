242afd401632d85b30947430efb95da274bf85ff
saubuny <122068609+saubuny@users.noreply.github.com>
2023-03-24 11:04:57 -0400

    Hello World in VGA Buffer

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,21 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Add an entrypoint for our program
 // Must be named _start exactly, uses C calling convention
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // 0xb8000 is the location of the VGA Buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
